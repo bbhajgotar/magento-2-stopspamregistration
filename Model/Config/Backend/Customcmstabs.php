23e3735f46f7b0409ce01a57bde77a7e8cebbfc6
Bharat Bhajgotar <bharat8640@gmail.com>
2024-01-02 15:41:49 +0530

    Issue fix: Incompatible argument type.

--- Model/Config/Backend/Customcmstabs.php
+++ Model/Config/Backend/Customcmstabs.php
@@ -21,19 +21,23 @@ class Customcmstabs extends \Magento\Framework\App\Config\Value
      * @param \Magento\Framework\Registry $registry
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
      * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
      * @param \Magento\Framework\Serialize\SerializerInterface $serializer
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\Model\Context $context,
         \Magento\Framework\Registry $registry,
-        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config, 
         \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
         \Magento\Framework\Serialize\SerializerInterface $serializer,
         array $data = []
     ) {
         $this->serializer = $serializer;
-        parent::__construct($context, $registry, $config, $cacheTypeList, $data);
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
     }
     
     /**
